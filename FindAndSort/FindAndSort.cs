f45cea7dfe0a273f9268f55c7241f61551048e16
lethienhongson96 <lethienhongson96@gmail.com>
2020-07-20 11:43:57 +0700

    asdas

---
+++ FindAndSort/FindAndSort.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindAndSort
+{
+    class FindAndSort
+    {
+        public int[] CreateArr(int side)
+        {
+            int[] arr = new int[side];
+            Random rnd = new Random();
+
+            for (int i = 0; i < side; i++)
+            {
+                arr[i] = rnd.Next(30, 60);
+            }
+
+            return arr;
+        }
+
+        public bool IsSymmetryArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != arr[arr.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = arr[i];
+                int indexMin = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (min > arr[j])
+                    {
+                        min = arr[j];
+                        indexMin = j;
+                    }
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[indexMin];
+                arr[indexMin] = temp;
+            }
+        }
+        public void PrintArr(int[] arr)
+        {
+            string str = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += $"{arr[i]} ";
+            }
+            Console.WriteLine(str);
+        }
+
+        public bool checkSorted(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] >= arr[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            this.SelectionSort(arr);
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int Find(int[] arr, int val)
+        {
+            if (!this.checkSorted(arr))
+            {
+                return -1;
+            }
+
+            int low = 0;
+            int high = arr.Length - 1;
+            while (high >= low)
+            {
+                int mid = (low + high) / 2;
+                if (val < arr[mid])
+                    high = mid - 1;
+                else if (val == arr[mid])
+                    return mid;
+                else
+                    low = mid + 1;
+            }
+            return -1;
+
+        }
+
+
+    }
+}
---
+++ FindAndSort/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindAndSort
+{
+    class Program
+    {
+        public static FindAndSort findAndSort = new FindAndSort();
+        public static int[] arr=new int[0];
+        static void Main(string[] args)
+        {
+            do
+            {
+
+
+                Menu();
+                int checkpress = Convert.ToInt32(Console.ReadLine());
+                if (checkpress==5)
+                {
+                    break;
+                }
+                switch (checkpress)
+                {
+                    case 1:
+                        Console.WriteLine("enter the side for array");
+                        int side = Convert.ToInt32(Console.ReadLine());
+                        arr = findAndSort.CreateArr(side);
+                        findAndSort.PrintArr(arr);
+                        break;
+
+                    case 2:
+                        if (!findAndSort.IsSymmetryArray(arr))
+                        {
+                            Console.WriteLine("not symmetry !!!");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("yes/ it is symmetry");
+                        }
+                        break;
+
+                    case 3:
+                        findAndSort.SelectionSort(arr);
+                        findAndSort.PrintArr(arr);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("enter the number you looking for: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        if (findAndSort.Find(arr,num)==-1)
+                        {
+                            Console.WriteLine("not found or not yet sort");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine($"found at {findAndSort.Find(arr, num)}");
+                        }
+                        break;
+                }
+            } while (true);
+
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("1/create arr");
+            Console.WriteLine("2/check Symmetry Array");
+            Console.WriteLine("3/sort arr");
+            Console.WriteLine("4/find index of value:");
+            Console.WriteLine("5/exits:");
+        }
+    }
+}
